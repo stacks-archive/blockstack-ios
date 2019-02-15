d7c379f305c7255ab46feba86cb1ba3222f51a52
Shreyas Thiagaraj <shreyas.thiagaraj@gmail.com>
2019-02-15 17:36:51 +0000

    Better hex method

--- Blockstack/Classes/Extensions.swift
+++ Blockstack/Classes/Extensions.swift
@@ -47,8 +47,14 @@ extension Data {
     }
     
     func hexEncodedString(options: HexEncodingOptions = []) -> String {
-        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
-        return map { String(format: format, $0) }.joined()
+        let hexDigits = Array((options.contains(.upperCase) ? "0123456789ABCDEF" : "0123456789abcdef").utf16)
+        var chars: [unichar] = []
+        chars.reserveCapacity(2 * count)
+        for byte in self {
+            chars.append(hexDigits[Int(byte / 16)])
+            chars.append(hexDigits[Int(byte % 16)])
+        }
+        return String(utf16CodeUnits: chars, count: chars.count)
     }
 }
 
